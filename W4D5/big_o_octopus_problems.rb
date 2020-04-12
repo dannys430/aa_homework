51750d8a766aca8a22873bb08a0d8825fb4cf3e1
Daniel Singer <dannys430@yahoo.com>
2020-04-12 19:06:53 -0400

    big_o_octopus_problems

--- W4D5/big_o_octopus_problems.rb
+++ W4D5/big_o_octopus_problems.rb
@@ -20,37 +20,73 @@ fishes = ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh', 'fsh
 
 
 # O(n * log(n)) dominant octopus
+# def dominant_octopus(arr, &prc)
+#     prc ||= Proc.new {|a, b| a.length <=> b.length}
 
-def dominant_octopus(arr, &prc)
-    prc ||= Proc.new {|a, b| a.length <=> b.length}
+#     return arr if arr.length < 2
 
-    return arr if arr.length < 2
+#     mid = arr.length / 2    # 4
+#     left = arr[0...mid]         # ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 
 
-    mid = arr.length / 2    # 4
-    left = arr[0...mid]         # ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 
+#     right = arr[mid..-1]        #                                           'fffish', 'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh']
 
-    right = arr[mid..-1]        #                                           'fffish', 'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh']
+#     sorted_left = dominant_octopus(left, &prc)
+#     sorted_right = dominant_octopus(right, &prc)
 
-    sorted_left = dominant_octopus(left, &prc)
-    sorted_right = dominant_octopus(right, &prc)
-
-    merge(sorted_left, sorted_right, &prc)
+#     merge(sorted_left, sorted_right, &prc)
     
-end
+# end
 
-def merge(left, right, &prc)
-    merged = []
-    until left.empty? || right.empty?
-        if prc.call(left.first, right.first) == -1
-            merged << left.shift
-        elsif prc.call(left.first, right.first) != -1
-            merged << right.shift
-        end
-    end
+# def merge(left, right, &prc)
+#     merged = []
+#     until left.empty? || right.empty?
+#         if prc.call(left.first, right.first) == -1
+#             merged << left.shift
+#         elsif prc.call(left.first, right.first) != -1
+#             merged << right.shift
+#         end
+#     end
 
-    merged + left + right
-end
+#     merged + left + right
+# end
 
-p dominant_octopus(fishes)[-1]
+# p dominant_octopus(fishes)[-1]
+
+
+# clever octopus - O(n) time
+# def clever(arr)
+#     longest = ""
+
+#     arr.each do |fish|
+#         longest = fish if fish.length > longest.length
+#     end
+
+#     longest
+# end
+
+# p clever(fishes)
+
+
+# DANCING OCTOPUS
+tiles_array = ["up", "right-up", "right", "right-down", "down", "left-down", "left",  "left-up" ]
+
+# #slow dance - O(n)
+# def slow(direction, array)
+#     array.each_with_index do |dir, idx|
+#         if dir == direction
+#             return idx
+#         end
+#     end
+#     nil
+# end
+
+# p slow("left-up", tiles_array)
+
+# #constant dance
+# def constant(direction, array)
+#     array.index(direction)
+# end
+
+# p constant("left-up", tiles_array)
 
 
